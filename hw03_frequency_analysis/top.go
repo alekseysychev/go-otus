4e633efa9ac5e56b05a2db3897f848e9c2294a52
Aleksey Sychev <79252045491@yandex.ru>
2021-02-25 08:05:47 +0300

    hw03

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,45 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type wordStatistic struct {
+	word  string //
+	count int    //
+}
+
+func Top10(text string) []string {
+	// словарь для пропуска элементов
+	skipWords := map[string]bool{
+		"-": true,
+	}
+
+	words := regexp.MustCompile(`[\p{L}\d-]+`).FindAllString(text, -1)
+
+	// собираем уникальный словарь
+	unique := map[string]int{}
+	for _, word := range words {
+		if _, ok := skipWords[word]; ok {
+			continue
+		}
+		unique[strings.ToLower(word)]++
+	}
+	// делаем из словаря срез
+	slice := []wordStatistic{}
+	for word, count := range unique {
+		slice = append(slice, wordStatistic{word, count})
+	}
+	// сортируем по количеству вхождений
+	sort.Slice(slice, func(i, j int) bool {
+		return slice[i].count > slice[j].count
+	})
+	// отбираем первые 10 элементов
+	result := []string{}
+	for i := 0; i < 10 && i < len(slice)-1; i++ {
+		result = append(result, slice[i].word)
+	}
+	return result
 }
